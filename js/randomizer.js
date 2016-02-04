70d6ea4fd7a3c86530aa1ad6253e3c4cc2f5e590
Keisha Perry <perry.keisham@gmail.com>
2016-02-04 08:30:22 -0800

    Fix missing layouts and styles

--- js/randomizer.js
+++ js/randomizer.js
@@ -1,18 +1,12 @@
     function generateRandomPosts()
     {
         $.getJSON("/random.json", function(data) {
-            console.log("[search.json loaded for random posts]");
-
             var postsCount = data.length;
             var posts = data;
-
             var randomIndexUsed = [];
             var counter = 0;
-            var numberOfPosts = 5;
-
-            var divRandomPosts = $("#random_posts");
-
-            divRandomPosts.append('<h2>Random posts</h2><hr />');
+            var numberOfPosts = 1;
+            var divRandomPosts = $(".random-posts");
 
             while (counter < numberOfPosts)
             {
@@ -22,21 +16,17 @@
                 {
                     var postHREF = posts[randomIndex].href;
                     var postTitle = posts[randomIndex].title;
-
-                    if (counter == (numberOfPosts - 1))
-                    {
-                        divRandomPosts.append('<p><a href="' + postHREF + '">' + postTitle + '</a></p>');
-                    }
-                    else
-                    {
-                        divRandomPosts.append('<p><a href="' + postHREF + '">' + postTitle + '</a></p><hr />');
-                    }
-
+                    var postAuthor = posts[randomIndex].author;
+                    var postContent = posts[randomIndex].content;
+                   
+                        divRandomPosts.append('<div class="article-header"><h2>' + postTitle + '</h2><h3>by: ' + postAuthor + '</h3></div>');
+                        divRandomPosts.append('<div class="article-content">' + postContent + '</div>');
                     randomIndexUsed.push(randomIndex);
 
                     counter++;
                 }
             }
+            divRandomPosts.append('</div>');
         });
     }
 
